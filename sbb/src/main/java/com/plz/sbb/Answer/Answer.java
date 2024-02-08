e13322ca05e78a190863dd17da0008fad631e045
hkak0594 <105026926+hkak0594@users.noreply.github.com>
2024-02-08 19:56:34 +0900

    Add files via upload

---
+++ sbb/src/main/java/com/plz/sbb/Answer/Answer.java
@@ -0,0 +1,36 @@
+package com.plz.sbb.Answer;
+
+
+import com.plz.sbb.User.SiteUser;
+import java.time.LocalDateTime;
+import com.plz.sbb.question.Question;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+
+    @ManyToOne
+    private SiteUser author;
+    private LocalDateTime modifyDate;
+}
---
+++ sbb/src/main/java/com/plz/sbb/question/Question.java
@@ -0,0 +1,42 @@
+package com.plz.sbb.question;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.plz.sbb.Answer.Answer;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+import com.plz.sbb.User.SiteUser;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+}
